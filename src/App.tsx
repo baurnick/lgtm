110fc00b2319000c8b43c281075c97f91ae1ca71
Nick Baur <79518844+baurnick@users.noreply.github.com>
2024-04-29 23:43:45 +0200

    feat: add link-input structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,24 @@ import ThemeToggle from './components/theme-toggle';
 function App() {
   return (
     <>
-      <div class="container mt-8">
+      <div class="container mt-8 max-w-[42rem]">
         <div class="flex justify-between items-center">
           <p class="text-base sm:text-lg md:text-xl lg:text-3xl font-mono font-bold">
             LOOKS GOOD TO ME | #lgtm
           </p>
           <ThemeToggle />
         </div>
-        <p class="text-sm mt-4 ">Pimp...</p>
-        <div class="flex justify-center items-center"></div>
+        <div class="flex justify-center items-center mt-8">
+          <div class="w-full max-w-[42rem]">
+            <input
+              type="text"
+              class="col-span-6 border ring-offset-background bg-stone-100 text-xs font-medium rounded-lg block w-full p-4 dark:bg-accent"
+              value="npm install flowbite"
+              disabled
+              readonly
+            />
+          </div>
+        </div>
       </div>
     </>
   );
