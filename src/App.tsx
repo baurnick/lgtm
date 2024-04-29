e400a4852cdc0e913bd04ab0c166656286ba81d7
Nick Baur <79518844+baurnick@users.noreply.github.com>
2024-04-29 23:19:40 +0200

    fix: change title

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,13 @@ function App() {
     <>
       <div class="container mt-8">
         <div class="flex justify-between items-center">
-          <p class="text-base sm:text-lg md:text-xl lg:text-4xl font-mono font-bold">
-            LOOKS GOOD TO ME
+          <p class="text-base sm:text-lg md:text-xl lg:text-3xl font-mono font-bold">
+            LOOKS GOOD TO ME | #lgtm
           </p>
           <ThemeToggle />
         </div>
-        <p class="text-sm mt-4 ">Pimp your MR's</p>
+        <p class="text-sm mt-4 ">Pimp...</p>
+        <div class="flex justify-center items-center"></div>
       </div>
     </>
   );
