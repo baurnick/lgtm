a3da3f3571243b10a0ed4bb87373c637e78dbd7d
Nick Baur <79518844+baurnick@users.noreply.github.com>
2024-05-05 11:52:50 +0200

    feat: add tooltip

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,32 @@ import Page from '@/components/page';
 import Card from '@/components/card';
 import { Input } from '@/components/ui/input';
 import { z } from 'zod';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import { convertToEmbeddedUrl } from '@/lib/utils';
 
 const urlSchema = z
   .string()
   .url()
   .refine((url) => url.endsWith('.gif'));
 
+interface Gif {
+  url: string;
+  embeded: string;
+}
+
 function App() {
   const numberOfGifs = gifs.gifs.length;
   const initialIndex = Math.floor(Math.random() * numberOfGifs);
 
-  const [gif, setGif] = React.useState(gifs.gifs[initialIndex]);
-  const [gifIndex, setGifIndex] = React.useState(initialIndex);
-  const [markdownLink, setMarkdownLink] = React.useState('');
-  const [inputValue, setInputValue] = React.useState('');
+  const [gif, setGif] = React.useState<Gif>(gifs.gifs[initialIndex]);
+  const [gifIndex, setGifIndex] = React.useState<number>(initialIndex);
+  const [markdownLink, setMarkdownLink] = React.useState<string>('');
+  const [inputValue, setInputValue] = React.useState<string>('');
   const [error, setError] = React.useState<string | null>(null);
 
   const lastGifIndex = React.useRef(initialIndex);
@@ -68,15 +80,6 @@ function App() {
     }
   };
 
-  const convertToEmbeddedUrl = (url: string) => {
-    const regex = /\/media\/.*\/(.*?)\/giphy\.gif$/;
-    const match = url.match(regex);
-
-    if (match && match[1]) {
-      return `https://giphy.com/embed/${match[1]}`;
-    }
-  };
-
   const clearInput = () => {
     setInputValue('');
     setError(null);
@@ -94,21 +97,36 @@ function App() {
               className="w-full sm:w-64 md:w-96 lg:w-128 aspect-square rounded-lg"
             />
             <div className="flex flex-row-reverse w-full mt-2">
-              <Button
-                variant="outline"
-                size="iconsm"
-                className="dark:bg-accent dark:hover:bg-card "
-                onClick={setCurrentGif}
-              >
-                <RefreshCcw className="h-3 w-3" />
-              </Button>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      variant="outline"
+                      size="iconsm"
+                      className="dark:bg-accent dark:hover:bg-card "
+                      onClick={setCurrentGif}
+                    >
+                      <RefreshCcw className="h-3 w-3" />
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <div className="flex items-center gap-2 text-xs font-thin font-mono">
+                      <p>Click</p>
+                      <div className=" text-white bg-slate-600 py-0.5 px-2 rounded-sm">
+                        <p>r</p>
+                      </div>
+                      <p>to randomize</p>
+                    </div>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
             </div>
           </Card>
           <Card className="flex justify-between">
             <p className="text-xs font-mono pr-8">Markdown to copy</p>
             <CopyToClipboardButton value={markdownLink} />
           </Card>
-          <div className="flex justify-between w-full items-center">
+          <div className="flex justify-between items-start w-full">
             <Input
               type="url"
               value={inputValue}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const convertToEmbeddedUrl = (url: string) => {
+  const regex = /\/media\/.*\/(.*?)\/giphy\.gif$/;
+  const match = url.match(regex);
+
+  if (match && match[1]) {
+    return `https://giphy.com/embed/${match[1]}`;
+  }
+};
