65b84ded491e0a492a63b834515d091e68c6324b
Nick Baur <79518844+baurnick@users.noreply.github.com>
2024-05-06 17:05:48 +0200

    fix: adapt margin of page

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -10,7 +10,7 @@ const Page: React.FC<PageProps> = ({ className, children }) => {
   return (
     <div
       className={cn(
-        'flex flex-col space-y-4 justify-center items-center mt-4 w-full max-w-[60rem]',
+        'flex flex-col space-y-4 justify-center items-center mt-14 w-full max-w-[60rem]',
         className
       )}
     >
